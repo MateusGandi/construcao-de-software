8064073abd81fb1579b679453c8c4e45beedecf6
mateus <mateusgandi3@gmail.com>
2024-04-19 19:14:33 -0300

    enums

--- tarefa03.java
+++ tarefa03.java
@@ -22,36 +22,3 @@ enum TipoEndereco{
     Residencial
 }
 
-public class PessoaFisica{
-    private String nome;
-private String sexo;
-private Date dataNascimento;
-private Endereco Endereco;
-
-}
-public class Bairro{
-    private String nome;
-    private Cidade cidade
-}
-
-public class Cidade{
-    private String nome;
-    private Estado estado
-}
-public class Estado{
-    private String sigla;
-    public String nome;
-    private Pais pais;
-}
-
-public class Pais{
-    public String nome
-}
-
-public class Endereco{
-    private int numero;
-    private String Complemento;
-    private int CEP;
-    private TipoEndereco tipoEndereco;
-    private Bairro bairro
-}
