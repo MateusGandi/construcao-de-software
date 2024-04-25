6e1f10ee962a36a6c8c44b2fd4d6810f3464ee78
VictorGelado <VictorGelado>
2024-04-25 20:51:41 -0300

    topico 2

---
+++ Atividade08/Topico02/main.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] vet = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int valor = -1;
+        
+        while (valor != 0) {
+            try {
+                System.out.print("Digite um valor: ");
+                valor = scanner.nextInt();
+
+                vet[count] = valor;
+                count++;
+            } catch (InputMismatchException e) {
+                System.out.println("Valor informado não é NaN.");
+                scanner.next(); 
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Passou o limite do vetor.");
+                break; 
+            }
+        }
+        scanner.close();
+    }
+}
+
