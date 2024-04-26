a4935d30b296027aecfe50818e467e4a37fc6a4d
VictorGelado <VictorGelado>
2024-04-25 21:25:15 -0300

    ultima, topico03

---
+++ Atividade08/Topico03/main.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+class LoginInvalidoException extends Exception {
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
+}
+
+class Login {
+    private String usuario;
+    private String senha;
+
+    public Login(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public boolean fazerLogin(String usuario, String senha) throws LoginInvalidoException{
+        if (this.usuario == usuario && this.senha == senha) {
+            return true;
+        } else {
+            throw new LoginInvalidoException("Usuário ou senha errada!");
+        }
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login("mateus", "1234");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Usuário: ");
+        String usuario = scanner.nextLine();
+        System.out.print("Senha: ");
+        String senha = scanner.nextLine();
+
+        try {
+            if (login.fazerLogin(usuario, senha)) {
+                System.out.println("Autenticação bem sucedida!");
+            }
+        } catch (LoginInvalidoException e) {
+            System.out.println("Erro no login: " + e.getMessage());
+        }
+        scanner.close(); 
+        
+    }
+}
+
+
